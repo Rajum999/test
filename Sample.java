3f80c97728893cb302b35097bf7c71d02a7392ec
Rajum999 <santhinireekshan@gmail.com>
2021-10-28 16:51:07 +0530

    the second change in java file

--- Sample.java
+++ Sample.java
@@ -3,7 +3,13 @@ package test;
 public class Sample {
 
 	public static void main(String[] args) {
+		int i=6;
+		if(i=6){
 		System.out.println("My name is Raju");
+		}
+		else{
+		System.out.println("My name is not Raju");
+		}
 
 	}
 
